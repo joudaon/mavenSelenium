32946f176d5b2cccdf2635316d5173ffcbc64de2
joudaon <jonuda77@gmail.com>
2017-03-07 16:55:50 +0100

    Maven Project Migration

---
+++ src/test/java/com/expedia/mavenSelenium/TestNG_TestSuite_26.java
@@ -0,0 +1,56 @@
+package com.expedia.mavenSelenium;
+
+import org.testng.annotations.Test;
+
+import page.classes.SearchPage;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+
+public class TestNG_TestSuite_26 {
+
+  private WebDriver driver;
+  private String baseUrl;
+  private static final Logger log = LogManager.getLogger(TestNG_TestSuite_26.class.getName());
+	
+  @BeforeClass
+  public void beforeClass() {
+	  driver = new FirefoxDriver();
+	  baseUrl = "https://www.expedia.com/";
+	  
+	  // Maximize the browser's window
+	  driver.manage().window().maximize();
+	  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	  driver.get(baseUrl);
+  }
+  
+  @Test
+  public void fillBasicInfo() throws Exception {	
+	SearchPage.flightTab(driver).click();
+	SearchPage.fillOriginTextBox(driver, "New York");
+	SearchPage.fillDestinationTextBox(driver, "Chicago");
+	SearchPage.fillDepartureDateTextBox(driver, "25/12/2017");
+	SearchPage.returnDateTextBox(driver).clear();
+	SearchPage.fillReturnDateTextBox(driver, "31/12/2017");
+	Thread.sleep(3000);
+  }
+  
+//  @Test
+//  public void fillAdvancedInfo(){
+//	  SearchPage.clickOnAdvancedLink(driver);
+//	  SearchPage.clickNonStopCheckBox(driver);
+//	  SearchPage.selectFlightClass(driver, "first");
+//  }
+
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+}
---
+++ src/test/java/page/classes/SearchPage.java
@@ -0,0 +1,174 @@
+package page.classes;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+//Search page = home page
+public class SearchPage {
+	public static WebElement element = null;
+	private static final Logger log = LogManager.getLogger(SearchPage.class.getName());
+	
+	/**
+	 * Clear all the fields on the Search page
+	 * @param driver
+	 */
+	public static void clearAllFields(WebDriver driver) {
+		driver.findElement(By.id("flight-origin")).clear();
+		driver.findElement(By.id("flight-destination")).clear();
+		driver.findElement(By.id("flight-departing")).clear();
+		driver.findElement(By.id("flight-returning")).clear();
+	}
+	
+	/**
+	 * Returns the Flights tab 
+	 * @param driver
+	 * @return
+	 */
+	public static WebElement flightTab(WebDriver driver) {
+		element = driver.findElement(By.id("tab-flight-tab"));
+		log.info("Navigate to flights tab");
+		return element;
+	}
+	
+	/**
+	 * Returns the flight origin text box element
+	 * @param driver
+	 * @return
+	 */
+	public static WebElement originTextBox(WebDriver driver) {
+		element = driver.findElement(By.id("flight-origin"));
+		log.info("Origin text box element found");
+		return element;
+	}
+	
+	public static void fillOriginTextBox(WebDriver driver, String origin) {
+		element = originTextBox(driver);
+		log.info("Enter origin city as " + origin);
+		element.sendKeys(origin);
+	}
+
+	/**
+	 * Returns the flight destination text box element
+	 * @param driver
+	 * @return
+	 */
+	public static WebElement destinationTextBox(WebDriver driver) {
+		element = driver.findElement(By.id("flight-destination"));
+		log.info("Destination text box element found");
+		return element;
+	}
+	
+	public static void fillDestinationTextBox(WebDriver driver, String destination) {
+		element = driver.findElement(By.id("flight-destination"));
+		log.info("Enter destination city as " + destination);
+		element.sendKeys(destination);
+	}
+
+
+	/**
+	 * Returns the departure date text box element
+	 * @param driver
+	 * @return
+	 */
+	public static WebElement departureDateTextBox(WebDriver driver) {
+		element = driver.findElement(By.id("flight-departing"));
+		return element;
+	}
+	
+	public static void fillDepartureDateTextBox(WebDriver driver, String departureDate) {
+		element = driver.findElement(By.id("flight-departing"));
+		element.sendKeys(departureDate);
+		log.info("Enter departure date as " + departureDate);
+	}
+
+	/**
+	 * Returns the return date text box element
+	 * @param driver
+	 * @return
+	 */
+	public static WebElement returnDateTextBox(WebDriver driver) {
+		element = driver.findElement(By.id("flight-returning"));
+		return element;
+	}
+	
+	public static void fillReturnDateTextBox(WebDriver driver, String returnDate) {
+		element = driver.findElement(By.id("flight-returning"));
+		element.sendKeys(returnDate);
+		log.info("Enter return date as " + returnDate);
+	}
+
+	/**
+	 * Returns the search button box element
+	 * @param driver
+	 * @return
+	 */
+	public static WebElement searchButton(WebDriver driver) {
+		element = driver.findElement(By.id("search-button"));
+		return element;
+	}
+
+	/**
+	 * Click on search button
+	 * @param driver
+	 */
+	public static void clickOnSearchButton(WebDriver driver) {
+		element = searchButton(driver);
+		element.click();
+	}
+	
+	/**
+	 * Navigate to flights tab
+	 * @param driver
+	 */
+	public static void navigateToFlightsTab(WebDriver driver) {
+		driver.findElement(By.id("header-history")).click();
+		element = driver.findElement(By.id("tab-flight-tab"));
+		element.click();
+	}
+	
+	/**
+	 * Click on Advanced Options link
+	 * @param driver
+	 */
+	public static void clickOnAdvancedLink(WebDriver driver) {
+		element = driver.findElement(By.id("advanced-options"));
+		element.click();
+		log.info("Clicked on Advanced Options link");
+	}
+	
+	/**
+	 * Click non-stop check box
+	 * @param driver
+	 */
+	public static void clickNonStopCheckBox(WebDriver driver) {
+		element = driver.findElement(By.id("advanced-flight-nonstop"));
+		element.click();
+		log.info("Clicked non-stop check box");
+	}
+	
+	/**
+	 * Select flight class
+	 * @param driver
+	 */
+	public static void selectFlightClass(WebDriver driver, String flightClass) {
+		Select options = new Select(driver.findElement(By.id("flight-advanced-preferred-class")));
+		options.selectByValue(flightClass);
+		log.info("Select flight class as " + flightClass);
+	}
+	
+	/**
+	 * Select flight adults
+	 * @param driver
+	 * @param flightAdults
+	 */
+	public static void selectFlightAdults(WebDriver driver, String flightAdults) {
+		Select options = new Select(driver.findElement(By.id("flight-adults")));
+		options.selectByValue(flightAdults);
+		log.info("Select flight adults as " + flightAdults);
+	}
+}
